90f97c10f59786140585dace1d5d111cf1cdb197
Aditya Sethi <adityasethi2004@gmail.com>
2024-10-23 03:36:48 +0530

    messages

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import {server as WebSocketServer} from "websocket"
+import {connection, server as WebSocketServer} from "websocket"
 import http from "http"
+import { IncomingMessage, SupportedMessageTypes } from "./messages/incomingMessages";
+import { OutgoingMessage, SupportedMessageTypes as OutgoingSupportedMessage} from "./messages/outgoingMessages";
+import { UserManager } from "./UserManager";
+import { InMemoryStore } from "./store/InMemoryStore";
 
-var server = http.createServer(function(request, response) {
+const server = http.createServer(function(request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
     response.writeHead(404);
     response.end();
 });
+
+const userManager = new UserManager();
+const store = new InMemoryStore();
 
 server.listen(8080, function() {
     console.log((new Date()) + ' Server is listening on port 8080');
@@ -13,33 +20,30 @@ server.listen(8080, function() {
 
 const wsServer = new WebSocketServer({
     httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
     autoAcceptConnections: false
 });
 
 function originIsAllowed(origin: string) {
-  // put logic here to detect whether the specified origin is allowed.
   return true;
 }
 
 wsServer.on('request', function(request) {
     if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
       request.reject();
       console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
       return;
     }
-    
+
     var connection = request.accept('echo-protocol', request.origin);
     console.log((new Date()) + ' Connection accepted.');
     connection.on('message', function(message) {
+        // Todo: add rate limiting logic here
         if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
+            try {
+                messageHandler(connection, JSON.parse(message.utf8Data));
+            } catch(error) {
+
+            }
         }
         else if (message.type === 'binary') {
             console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
@@ -50,3 +54,37 @@ wsServer.on('request', function(request) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
+
+function messageHandler(ws: connection, message: IncomingMessage) {
+    if (message.type == SupportedMessageTypes.JoinRoom) {
+        const payload = message.payload;
+        userManager.addUser(payload.userId, payload.roomId, payload.name, ws);
+    }
+
+    if (message.type == SupportedMessageTypes.SendMessage) {
+        const payload = message.payload;
+        const user = userManager.getUser(payload.userId, payload.roomId);
+
+        if (!user) {
+            console.error("User not found");
+            return;
+        }
+
+        store.addChat(payload.userId, user.name, payload.roomId, payload.message);
+        const outgoingPayload: OutgoingMessage = {
+            type: OutgoingSupportedMessage.AddChat,
+            payload: {
+                roomId: payload.roomId,
+                message: payload.message,
+                name: user.name,
+                upvotes: 0
+            }
+        }
+        userManager.broadcast(payload.roomId, payload.userId, outgoingPayload);
+    }
+
+    if (message.type == SupportedMessageTypes.UpvoteMessage) {
+        const payload = message.payload;
+        store.upvote(payload.userId, payload.roomId, payload.chatId);
+    }
+}
---
+++ src/messages/outgoingMessages.ts
@@ -0,0 +1,20 @@
+
+export enum SupportedMessageTypes {
+    AddChat = "ADD_CHAT",
+    UpdateChat = "UPDATE_CHAT",
+}
+
+type MessagePayload = {
+    roomId: string;
+    message: string;
+    name: string;
+    upvotes: number;
+}
+
+export type OutgoingMessage = {
+    type: SupportedMessageTypes.AddChat,
+    payload: MessagePayload
+} | {
+    type: SupportedMessageTypes.UpdateChat,
+    payload: Partial<MessagePayload>
+}

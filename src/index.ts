fbcc8aa043a18c2105a420d8322dd09d0c6caecf
Aditya Sethi <adityasethi2004@gmail.com>
2024-10-23 03:51:22 +0530

    added msg handler

--- src/index.ts
+++ src/index.ts
@@ -52,7 +52,7 @@ wsServer.on('request', function(request) {
     });
     connection.on('close', function(reasonCode, description) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
+    }); 
 });
 
 function messageHandler(ws: connection, message: IncomingMessage) {

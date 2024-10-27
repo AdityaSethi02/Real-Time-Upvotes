377a87fa31bb89d21f66d133f41c5bee2fb7e70b
Aditya Sethi <adityasethi2004@gmail.com>
2024-10-27 09:55:51 +0530

    changed ws url

--- chat-frontend/src/app/myComponent.tsx
+++ chat-frontend/src/app/myComponent.tsx
@@ -57,7 +57,7 @@ export default function MyComponent({
     }
 
     useEffect(() => {
-        const ws = new WebSocket("ws://localhost:8787/", "echo-protocol");
+        const ws = new WebSocket("ws://localhost:8080/", "echo-protocol");
         setSocket(ws);
 
         ws.onopen = function () {

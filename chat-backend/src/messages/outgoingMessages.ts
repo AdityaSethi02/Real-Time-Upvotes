05706eeec7aa406ab4645150c6ed592e2dae4a50
Aditya Sethi <adityasethi2004@gmail.com>
2024-10-30 02:52:08 +0530

    name changes

--- chat-backend/src/messages/outgoingMessages.ts
+++ chat-backend/src/messages/outgoingMessages.ts
@@ -1,7 +1,7 @@
 
 export enum SupportedMessage {
     AddChat = "ADD_CHAT",
-    UpdateChat = "UPDATE_CHAT"
+    UpdateChat = "UPDATE_CHAT",
 }
 
 type MessagePayload = {
--- chat-frontend/src/app/myComponent.tsx
+++ chat-frontend/src/app/myComponent.tsx
@@ -111,7 +111,7 @@ export default function MyComponent({
     }
 
     useEffect(() => {
-        const ws = new WebSocket("wss://chatboard-backend-df7b097168cd.herokuapp.com/");
+        const ws = new WebSocket("ws://localhost:8080/");
         setSocket(ws);
 
         ws.onopen = function () {

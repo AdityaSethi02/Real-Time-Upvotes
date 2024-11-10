8c8d362846129ff0d73faf01ca8bbc4301c60d88
Aditya Sethi <adityasethi2004@gmail.com>
2024-11-10 20:14:43 +0530

    few checks

--- chat-frontend/src/app/myComponent.tsx
+++ chat-frontend/src/app/myComponent.tsx
@@ -111,7 +111,7 @@ export default function MyComponent({
     }
 
     useEffect(() => {
-        const ws = new WebSocket("wss://chatboard-backend-df7b097168cd.herokuapp.com/");
+        const ws = new WebSocket("wss://upvote-backend.vercel.app/");
         setSocket(ws);
 
         ws.onopen = function () {

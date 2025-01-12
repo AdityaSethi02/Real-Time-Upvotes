3e205d32c5a383eeb033f8613686dfa5de2d6f19
Aditya Sethi <adityasethi2004@gmail.com>
2025-01-13 05:11:42 +0530

    url

--- chat-frontend/src/components/MainPage.tsx
+++ chat-frontend/src/components/MainPage.tsx
@@ -29,7 +29,6 @@ export default function MainPage({ initialChats, upVotes1 = 3, upVotes2 = 10 }: 
 	const [isModalOpen, setIsModalOpen] = useState(false);
 	const [copy, setCopy] = useState(false);
     const [roomData, setRoomData] = useState<roomData | null>(null);
-    const WS_BACKEND_URL = process.env.WS_BACKEND_URL;
 
     const { roomId } = useParams();
 
@@ -152,12 +151,7 @@ export default function MainPage({ initialChats, upVotes1 = 3, upVotes2 = 10 }: 
     }
 
     useEffect(() => {
-        if (!WS_BACKEND_URL) {
-            console.error("WS_BACKEND_URL is not defined");
-            return;
-        }
-
-        const ws = new WebSocket(WS_BACKEND_URL);
+        const ws = new WebSocket("wss://upvote-backend.onrender.com/");
         setSocket(ws);
 
         ws.onopen = function () {
